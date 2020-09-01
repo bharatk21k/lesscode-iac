a60bcb0c3a650bb98778144b2ff5a8b0e2b01317
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-09-01 15:21:00 -0700

    added cf

--- microservice/aws/routing/cloudfront.tf
+++ microservice/aws/routing/cloudfront.tf
@@ -19,15 +19,19 @@ resource "aws_cloudfront_distribution" "default" {
     target_origin_id = var.alb_dns_name
 
     forwarded_values {
+      query_string = true
+      headers      = ["Origin"]
+
       cookies {
-        forward = "all"
+        forward = "none"
       }
     }
 
-    viewer_protocol_policy = "allow-all"
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
   }
 
   tags = {
