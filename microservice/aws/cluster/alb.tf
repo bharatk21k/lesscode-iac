962807bb9608ba8e00643766df947e2ad76d5d4e
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-09-03 19:55:34 -0700

    subnet ids

--- microservice/aws/cluster/alb.tf
+++ microservice/aws/cluster/alb.tf
@@ -2,7 +2,7 @@
 
 resource "aws_alb" "main" {
   name            = "${var.ecs_cluster_name}-load-balancer"
-  subnets         = aws_subnet.private.*.id
+  subnets         = aws_subnet.public.*.id
   security_groups = [aws_security_group.lb.id]
   tags = {
     Name = var.ecs_cluster_name
--- microservice/aws/cluster/network.tf
+++ microservice/aws/cluster/network.tf
@@ -22,12 +22,18 @@ resource "aws_subnet" "private" {
   }
 }
 
+# Create var.az_count public subnets, each in a different AZ
+resource "aws_subnet" "public" {
+  count                   = var.az_count
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+}
+
 # Internet Gateway for the public subnet
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
-  tags = {
-    Name = var.ecs_cluster_name
-  }
 }
 
 # Route the public subnet traffic through the IGW
@@ -35,9 +41,6 @@ resource "aws_route" "internet_access" {
   route_table_id         = aws_vpc.main.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
-  tags = {
-    Name = var.ecs_cluster_name
-  }
 }
 
 # Create a NAT gateway with an Elastic IP for each private subnet to get internet connectivity
@@ -45,18 +48,12 @@ resource "aws_eip" "gw" {
   count      = var.az_count
   vpc        = true
   depends_on = [aws_internet_gateway.gw]
-  tags = {
-    Name = var.ecs_cluster_name
-  }
 }
 
 resource "aws_nat_gateway" "gw" {
   count         = var.az_count
   subnet_id     = element(aws_subnet.public.*.id, count.index)
   allocation_id = element(aws_eip.gw.*.id, count.index)
-  tags = {
-    Name = var.ecs_cluster_name
-  }
 }
 
 # Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
@@ -68,9 +65,6 @@ resource "aws_route_table" "private" {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = element(aws_nat_gateway.gw.*.id, count.index)
   }
-  tags = {
-    Name = var.ecs_cluster_name
-  }
 }
 
 # Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
@@ -78,8 +72,4 @@ resource "aws_route_table_association" "private" {
   count          = var.az_count
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(aws_route_table.private.*.id, count.index)
-
-  tags = {
-    Name = var.ecs_cluster_name
-  }
 }
