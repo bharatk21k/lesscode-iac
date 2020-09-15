4bab0c0c8faeed5af37def3cd404c6d64145aaca
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-09-15 12:33:51 -0700

    adding count metrics

--- microservice/aws/service/api/metrics.tf
+++ microservice/aws/service/api/metrics.tf
@@ -14,4 +14,18 @@ resource "aws_cloudwatch_log_metric_filter" "latency" {
   }
 }
 
+resource "aws_cloudwatch_log_metric_filter" "count" {
+  count = length(var.metrics)
+  name  = "${data.aws_ecs_cluster.ecs.cluster_name}-${var.name}-${var.metrics[count.index]}-count"
+
+  pattern        = "{$.status = ${var.metrics[count.index]}*}"
+  log_group_name = aws_cloudwatch_log_group.service_log_group.name
+
+  metric_transformation {
+    name      = "count-${var.metrics[count.index]}"
+    namespace = data.aws_ecs_cluster.ecs.cluster_name
+    value     = "1"
+  }
+}
+
 
