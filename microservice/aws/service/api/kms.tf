09aaaa67538950371b20133edd9efc5e6e3a7dd9
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-09-17 15:45:00 -0700

    addming KMS

--- microservice/aws/service/api/kms.tf
+++ microservice/aws/service/api/kms.tf
@@ -7,3 +7,11 @@ resource "aws_kms_alias" "main" {
   name          = "alias/${data.aws_ecs_cluster.ecs.cluster_name}"
   target_key_id = aws_kms_key.main.key_id
 }
+
+resource "aws_kms_grant" "a" {
+  name              = "my-grant"
+  key_id            = aws_kms_key.main.key_id
+  grantee_principal = aws_iam_role.a.arn
+  operations        = ["Encrypt", "Decrypt", "GenerateDataKey"]
+
+}
--- microservice/aws/service/api/roles.tf
+++ microservice/aws/service/api/roles.tf
@@ -16,7 +16,30 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
 # ECS task execution role
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "${var.ecs_cluster_name}-${var.name}-EcsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+   assume_role_policy = <<EOF
+   {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Effect": "Allow",
+              "Action": [
+                  "ecr:GetAuthorizationToken",
+                  "ecr:BatchCheckLayerAvailability",
+                  "ecr:GetDownloadUrlForLayer",
+                  "ecr:BatchGetImage",
+                  "logs:CreateLogStream",
+                  "logs:PutLogEvents",
+                  "kms:Encrypt",
+                  "kms:Decrypt",
+                  "kms:ReEncrypt*",
+                  "kms:GenerateDataKey*",
+                  "kms:DescribeKey"
+              ],
+              "Resource": "*"
+          }
+        ]
+    }
+    EOF
 }
 
 # ECS task execution role policy attachment
