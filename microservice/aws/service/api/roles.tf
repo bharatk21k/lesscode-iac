7f6a0976d9e92fea2d647d5e48827e4c7d618531
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-09-17 16:08:52 -0700

    addming KMS

--- microservice/aws/service/api/roles.tf
+++ microservice/aws/service/api/roles.tf
@@ -1,18 +1,3 @@
-# ECS task execution role data
-data "aws_iam_policy_document" "ecs_task_execution_role" {
-  version = "2012-10-17"
-  statement {
-    sid = ""
-    effect = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
 # ECS task execution role
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "${var.ecs_cluster_name}-${var.name}-EcsTaskExecutionRole"
