0caeff3ccffd59e8c77c8162b3246387c978467b
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-09-17 14:32:37 -0700

    addming KMS

--- microservice/aws/service/api/kms.tf
+++ microservice/aws/service/api/kms.tf
@@ -1,7 +1,6 @@
 resource "aws_kms_key" "main" {
   description             = data.aws_ecs_cluster.ecs.cluster_name
   deletion_window_in_days = 10
-  enable_key_rotation = true
 }
 
 resource "aws_kms_alias" "main" {
