c57a973e75995c58d96e00b4d29cb42126dca88c
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-09-17 16:36:04 -0700

    addming KMS

--- microservice/aws/service/api/roles.tf
+++ microservice/aws/service/api/roles.tf
@@ -21,5 +21,5 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 # ECS task execution role policy attachment
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  policy_arn = data.aws_iam_policy_document.ecs_task_execution_role.arn
 }
