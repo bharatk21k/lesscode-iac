91eeb779bb6d58d1c256855bac3b27e6d3fd1ca4
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-09-17 17:04:39 -0700

    addming KMS

--- microservice/aws/service/api/roles.tf
+++ microservice/aws/service/api/roles.tf
@@ -4,11 +4,20 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
   statement {
     sid = ""
     effect = "Allow"
-    actions = ["sts:AssumeRole"]
+    actions = ["sts:AssumeRole","kms:Encrypt"]
     principals {
       type        = "Service"
       identifiers = ["ecs-tasks.amazonaws.com"]
     }
+  }
+
+  statement {
+    sid = ""
+    effect = "Allow"
+    actions = ["kms:Encrypt"]
+    resources = [
+      "arn:aws:kms:::*",
+    ]
   }
 }
 
