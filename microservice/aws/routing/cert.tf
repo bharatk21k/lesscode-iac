8b1ea3f2f5d1c021bc79c3479c98ee22c5c785d4
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-11-04 09:07:53 -0800

    fixed TLS certificate issue

--- microservice/aws/routing/cert.tf
+++ microservice/aws/routing/cert.tf
@@ -13,7 +13,7 @@ resource "aws_acm_certificate" "default"{
   }
 }
 
-resource "aws_route53_record" "default" {
+resource "aws_route53_record" "existing" {
   for_each = {
     for dvo in aws_acm_certificate.default.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -32,5 +32,5 @@ resource "aws_route53_record" "default" {
 
 resource "aws_acm_certificate_validation" "default" {
   certificate_arn         = aws_acm_certificate.default.arn
-  validation_record_fqdns = [for record in aws_route53_record.default : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.existing : record.fqdn]
 }
