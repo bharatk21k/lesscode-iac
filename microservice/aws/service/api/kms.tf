46a4569969aea7f1a170bc85ce6c428a43955cde
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-11-16 14:46:40 -0800

    KMS security

--- microservice/aws/service/api/kms.tf
+++ microservice/aws/service/api/kms.tf
@@ -2,22 +2,31 @@ resource "aws_kms_key" "main" {
   description             = data.aws_ecs_cluster.ecs.cluster_name
   deletion_window_in_days = 10
   policy = <<POLICY
-  {
-    "Version": "2012-10-17",
-    "Id": "key-default-1",
-    "Statement": [
-        {
-            "Sid": "Enable IAM User Permissions",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "${var.task_role_arn}"
-            },
-            "Action": "kms:*",
-            "Resource": "*"
-        }
-    ]
-  }
-  POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "key-default-1",
+  "Statement": [
+      {
+          "Sid": "Enable IAM User Permissions",
+          "Effect": "Allow",
+          "Principal": {
+              "AWS": "arn:aws:iam::092166348842:root"
+          },
+          "Action": "kms:*",
+          "Resource": "*"
+      },
+      {
+          "Sid": "Enable IAM User Permissions",
+          "Effect": "Allow",
+          "Principal": {
+              "AWS": "${var.task_role_arn}"
+          },
+          "Action": "kms:*",
+          "Resource": "*"
+      }
+  ]
+}
+POLICY
 }
 
 resource "aws_kms_alias" "main" {
