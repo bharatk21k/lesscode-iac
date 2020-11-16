fd2bc55504a0eb4de58afb72f844c59c01c0b2f5
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-11-16 09:23:40 -0800

    removing unnecessary metrics dimension

--- microservice/aws/service/api/main.tf
+++ microservice/aws/service/api/main.tf
@@ -57,7 +57,7 @@ data "template_file" "service" {
 
 resource "aws_ecs_task_definition" "task" {
   family                   = var.name
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = "arn:aws:iam::092166348842:role/service"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = var.fargate_cpu
