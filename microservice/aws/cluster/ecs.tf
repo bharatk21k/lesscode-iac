8e5655f406878100a5f81c4e0dbbc66978668bef
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2021-01-04 16:49:37 -0800

    adding cluster insight

--- microservice/aws/cluster/ecs.tf
+++ microservice/aws/cluster/ecs.tf
@@ -3,7 +3,8 @@
 resource "aws_ecs_cluster" "main" {
   name = "${var.ecs_cluster_name}-cluster"
   setting {
-    container_insight = true
+    name = "container_insight"
+    value = "enabled"
   }
 }
 
