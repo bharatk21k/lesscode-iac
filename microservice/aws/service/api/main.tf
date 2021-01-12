010d11ab3462082e426f9ebb36ddfdbaebde4e51
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2021-01-12 10:26:42 -0800

    adding env name

--- microservice/aws/service/api/main.tf
+++ microservice/aws/service/api/main.tf
@@ -42,6 +42,7 @@ resource "aws_cloudwatch_log_stream" "service_log_stream" {
 data "template_file" "service" {
   template = file("${path.module}/templates/ecs/taskdef.json.tpl")
   vars = {
+    env                 = var.env
     name                = var.name
     ecs_cluster_name    = var.ecs_cluster_name
     service_image       = var.service_image
