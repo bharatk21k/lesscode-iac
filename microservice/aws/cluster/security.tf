695d12bb688ba9e0508891db8ae4ff585b4c0dd4
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2021-01-16 15:24:47 -0800

    adding ingress rule for 80

--- microservice/aws/cluster/security.tf
+++ microservice/aws/cluster/security.tf
@@ -8,6 +8,13 @@ resource "aws_security_group" "lb" {
 
   ingress {
     protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
     from_port   = var.tls_port
     to_port     = var.tls_port
     cidr_blocks = ["0.0.0.0/0"]
