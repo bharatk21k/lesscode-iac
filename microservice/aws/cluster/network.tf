82733fbf9607d8d0a0e429fc365de573548befe9
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2021-01-26 11:54:23 -0800

    allowing vpc id to be passed

--- microservice/aws/cluster/network.tf
+++ microservice/aws/cluster/network.tf
@@ -17,36 +17,36 @@ data "aws_vpc" "main" {
 }
 
 # Create var.az_count private subnets, each in a different AZ
-resource "aws_subnet" "private" {
-  count             = var.az_count
-  cidr_block        = cidrsubnet(data.aws_vpc.main.cidr_block, 8, count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  vpc_id            = data.aws_vpc.main.id
-  tags = {
-    Name = var.ecs_cluster_name
-  }
-}
+#resource "aws_subnet" "private" {
+#  count             = var.az_count
+#  cidr_block        = cidrsubnet(data.aws_vpc.main.cidr_block, 8, count.index)
+#  availability_zone = data.aws_availability_zones.available.names[count.index]
+#  vpc_id            = data.aws_vpc.main.id
+#  tags = {
+#    Name = var.ecs_cluster_name
+#  }
+#}
 
 # Create var.az_count public subnets, each in a different AZ
-resource "aws_subnet" "public" {
-  count                   = var.az_count
-  cidr_block              = cidrsubnet(data.aws_vpc.main.cidr_block, 8, var.az_count + count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  vpc_id                  = data.aws_vpc.main.id
-  map_public_ip_on_launch = true
-}
+#resource "aws_subnet" "public" {
+#  count                   = var.az_count
+#  cidr_block              = cidrsubnet(data.aws_vpc.main.cidr_block, 8, var.az_count + count.index)
+#  availability_zone       = data.aws_availability_zones.available.names[count.index]
+#  vpc_id                  = data.aws_vpc.main.id
+#  map_public_ip_on_launch = true
+#}
 
 # Internet Gateway for the public subnet
-resource "aws_internet_gateway" "gw" {
-  vpc_id = data.aws_vpc.main.id
-}
+#resource "aws_internet_gateway" "gw" {
+# vpc_id = data.aws_vpc.main.id
+#}
 
 # Route the public subnet traffic through the IGW
-resource "aws_route" "internet_access" {
-  route_table_id         = data.aws_vpc.main.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.gw.id
-}
+#resource "aws_route" "internet_access" {
+#  route_table_id         = data.aws_vpc.main.main_route_table_id
+#  destination_cidr_block = "0.0.0.0/0"
+#  gateway_id             = aws_internet_gateway.gw.id
+#}
 
 # Create a NAT gateway with an Elastic IP for each private subnet to get internet connectivity
 #resource "aws_eip" "gw" {
