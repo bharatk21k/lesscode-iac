00efe35e2bd97c0fd6f9f69c56b048c539cec980
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2021-03-09 13:11:27 -0800

    adding report port 3000 to ecs

--- microservice/aws/cluster/alb.tf
+++ microservice/aws/cluster/alb.tf
@@ -26,6 +26,24 @@ resource "aws_lb_listener" "redirect" {
   }
 }
 
+resource "aws_alb_listener" "report" {
+  load_balancer_arn = aws_alb.main.id
+  port              = "3000"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate_validation.default.certificate_arn
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<html><h2>Powered by <a href='https://github.com/van001/lesscode-iac'> Lesscode! </a></h2></html>"
+      status_code  = "200"
+    }
+  }
+}
+
 resource "aws_alb_listener" "default" {
   load_balancer_arn = aws_alb.main.id
   port              = "443"
