9473828c78c50b62b4a82967c17b617f42e13cf9
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2021-05-18 14:45:18 -0700

    adding svc

--- microservice/aws/service/svc/vars.tf
+++ microservice/aws/service/svc/vars.tf
@@ -10,26 +10,26 @@ variable "env" {
 
 variable "ecs_cluster_name" {
     type = string
-    default = "dev"
+    #default = "dev"
 }
 
 variable "name" {
     type = string
-    default = "improvisor2"
+    #default = "improvisor2"
 }
 
 
 variable "retention" {
     type = string
-    default = "86400"
+    #default = "86400"
 }
 
 variable "visibility" {
     type = string
-    default = "300"
+    #default = "300"
 }
 
 variable "dlq" {
     type = string
-    default = "arn:aws:sqs:us-west-2:092166348842:dlq-dev"
+    #default = "arn:aws:sqs:us-west-2:092166348842:dlq-dev"
 }
