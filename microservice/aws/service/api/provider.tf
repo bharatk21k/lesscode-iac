bb6afdc93187ad10417e14cb22b6dd0d3e6f846e
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-MacBook-Pro.local>
2022-02-11 14:23:51 -0800

    updating api credential file

--- microservice/aws/service/api/provider.tf
+++ microservice/aws/service/api/provider.tf
@@ -3,7 +3,7 @@
 
 # Specify the provider and access details
 provider "aws" {
-  AWS_SHARED_CREDENTIALS_FILE = "/Users/me/.aws/credentials"
+  shared_credentials_file = "/Users/me/.aws/credentials"
   profile                 = "default"
   region                  = var.region
 }
