d4f7aaaa753da42c17e831d83064b7a97f5f58a6
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-MacBook-Pro.local>
2022-02-22 14:39:07 -0800

    updated dynamodb name

--- microservice/aws/storage/dynamodb/main.tf
+++ microservice/aws/storage/dynamodb/main.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "defaults" {
-  name         = "${var.name}-${ecs_cluster_name}"
+  name         = "${var.name}-${var.ecs_cluster_name}"
   billing_mode = var.billing_mode
   hash_key     = var.hash_key
   range_key    = var.range_key
