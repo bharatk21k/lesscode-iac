539dd9a172515a225ec00d367005c35cc33c9333
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-MacBook-Pro.local>
2022-03-03 01:31:45 +0530

    removed role arn from cloudwatc sub

--- microservice/aws/subscription/cloudwatch/main.tf
+++ microservice/aws/subscription/cloudwatch/main.tf
@@ -1,6 +1,5 @@
 resource "aws_cloudwatch_log_subscription_filter" "subscription" {
   name            = var.name
-  role_arn        = var.task_role_arn
   log_group_name  = "service/${var.ecs_cluster_name}/${var.service_name}"
   filter_pattern  = var.filter_pattern
   destination_arn = var.service_arn
