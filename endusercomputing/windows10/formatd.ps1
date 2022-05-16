eef8c5f8f611e79fd46d82425e5c4f44c25027cc
Sudhakar Chundu <32757211+schunduEA@users.noreply.github.com>
2022-05-16 22:07:55 +0530

    Delete formatd.ps1

--- endusercomputing/windows10/formatd.ps1
+++
@@ -1,2 +0,0 @@
-
-Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -UseMaximumSize -DriveLetter D | Format-Volume -FileSystem NTFS -NewFileSystemLabel "data" -Confirm:$false"
