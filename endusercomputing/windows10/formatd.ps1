a0339c421c485f1b7f405047ce7b73a3e4d42465
Sudhakar Chundu <32757211+schunduEA@users.noreply.github.com>
2022-05-16 19:15:36 +0530

    Update formatd.ps1

--- endusercomputing/windows10/formatd.ps1
+++ endusercomputing/windows10/formatd.ps1
@@ -1,1 +1,2 @@
 
+Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -UseMaximumSize -DriveLetter D | Format-Volume -FileSystem NTFS -NewFileSystemLabel "data" -Confirm:$false"
