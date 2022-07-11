6f8e501e7ac4f52e535d97cebc3a98eeeb08a6b9
Sudhakar Chundu <32757211+schunduEA@users.noreply.github.com>
2022-07-12 01:19:09 +0530

    Update vars.tf

--- monitoring/pingdom/vars.tf
+++ monitoring/pingdom/vars.tf
@@ -52,9 +52,9 @@ variable "enabled" {
 variable "status" {
   type = string
   description = "Status Contains"
-  default = <<EOF
-ok
-EOF
+#  default = <<EOF
+#ok
+#EOF
 }
 
 variable "custom_message" {
