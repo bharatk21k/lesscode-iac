0a04faa2e9968aad75ded30b9e354070e5e765b4
neeleshhenkel <neelesh@albeertinvent.com>
2022-07-19 17:01:21 -0700

    enabled kms key rotation

--- microservice/aws/security/kms/main.tf
+++ microservice/aws/security/kms/main.tf
@@ -9,7 +9,7 @@ resource "aws_kms_key" "primary" {
   description             = "Multi-Region primary key for ${var.tenant_id}"
   deletion_window_in_days = 7
   multi_region            = true
-  //enabled                 = true
+  enable_key_rotation = true
 
   //key_material_base64 = var.key
 }
