11f319b62341497531b0db8c10c51136f7238d21
neeleshhenkel <neelesh@albeertinvent.com>
2022-07-19 20:42:21 -0700

    updated kms alias to kms key

--- microservice/aws/storage/dynamodb/main.tf
+++ microservice/aws/storage/dynamodb/main.tf
@@ -118,6 +118,6 @@ resource "aws_dynamodb_table" "defaults" {
 
   server_side_encryption{
     enabled = true
-    kms_key_arn = data.aws_kms_alias.partition.arn
+    kms_key_arn = data.aws_kms_key.partition.arn
   }
 }
