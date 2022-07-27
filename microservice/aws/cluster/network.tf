2c0cedeb333150a1fcc4d19d520b2408b69a0ce2
neeleshhenkel <neelesh@albeertinvent.com>
2022-07-27 10:08:27 -0700

    remving NAT gateway

--- microservice/aws/cluster/network.tf
+++ microservice/aws/cluster/network.tf
@@ -54,26 +54,26 @@ resource "aws_eip" "gw" {
   depends_on = [aws_internet_gateway.gw]
 }
 
-resource "aws_nat_gateway" "gw" {
-  count         = var.az_count
-  subnet_id     = element(aws_subnet.public.*.id, count.index)
-  allocation_id = element(aws_eip.gw.*.id, count.index)
+#resource "aws_nat_gateway" "gw" {
+#  count         = var.az_count
+ # subnet_id     = element(aws_subnet.public.*.id, count.index)
+ # allocation_id = element(aws_eip.gw.*.id, count.index)
 }
 
 # Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
-resource "aws_route_table" "private" {
-  count  = var.az_count
-  vpc_id = aws_vpc.main.id
+#resource "aws_route_table" "private" {
+#  count  = var.az_count
+ # vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = element(aws_nat_gateway.gw.*.id, count.index)
-  }
-}
+ # route {
+  #  cidr_block     = "0.0.0.0/0"
+ #   nat_gateway_id = element(aws_nat_gateway.gw.*.id, count.index)
+ # }
+#}
 
 # Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
-resource "aws_route_table_association" "private" {
- count          = var.az_count
-  subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = element(aws_route_table.private.*.id, count.index)
-}
+#resource "aws_route_table_association" "private" {
+ #count          = var.az_count
+ # subnet_id      = element(aws_subnet.private.*.id, count.index)
+  #route_table_id = element(aws_route_table.private.*.id, count.index)
+#}
